96f1f92dc3b71ada34d23f855160240e7c3b4490
Daniel Onyemelukwe <danielonye.edu@gmail.com>
2023-05-24 22:17:38 -0500

    Corrected syntax errors and added additional game logic in script.js

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@
  * @return 'rock', 'paper', or 'scissors'
 */
 function getComputerChoice() {
-    let options = ['rock', 'paper', 'scissors']
-    let choice = Math.floor(Math.random()*options.length)
+    let options = ['rock', 'paper', 'scissors'];
+    let choice = Math.floor(Math.random()*options.length);
 
     return options[choice]
 }
@@ -15,72 +15,124 @@ function getComputerChoice() {
  * Plays Round of the game
  * @param playerSelection
  * @param computerSelection
- * @return string declaring the winner of the round
+ * @return 1 if player wins, 2 if the computer wins, or 3 if it is a draw
  */
 function playerRound(playerSelection, computerSelection) {
     let playerWins; // True if player wins, False if player loses
-    let draw = false // True if there is a draw
+    let draw = false;; // True if there is a draw
     let decision; // string of round outcome
 
     //If player chooses rock
     if (playerSelection === "rock"){
         if (computerSelection === "scissors"){
-            playerWins = true // rock beats scissors
+            playerWins = true; // rock beats scissors
         }
         else if (computerSelection === "rock"){
-            draw = true // rock equals rock
+            draw = true; // rock equals rock
         }
         else {
-            playerWins = false // paper beats rock
+            playerWins = false; // paper beats rock
         }
     }
     //If player chooses paper
     else if (playerSelection === "paper"){
         if (computerSelection === "scissors"){
-            playerWins = false // scissors beats paper
+            playerWins = false; // scissors beats paper
         }
         else if (computerSelection === "rock"){
-            playerWins = true // paper beats rock
+            playerWins = true; // paper beats rock
         }
         else {
-            draw = true // paper equals paper
+            draw = true; // paper equals paper
         }
     }
     //If player chooses scissors
     else {
         if (computerSelection === "scissors"){
-            draw = true // scissors equals scissors
+            draw = true; // scissors equals scissors
         }
         else if (computerSelection === "rock"){
-            playerWins = false // rock beats scissors
+            playerWins = false; // rock beats scissors
         }
         else {
-            playerWins = true // scissors beats paper
+            playerWins = true; // scissors beats paper
         }
     }
 
     // Check if draw first, then if player won
     if (draw) {
-        decision = `There is a draw, both players chose ${playerSelection}`
+        decision = 3;
+        console.log(`There is a draw, both players chose ${playerSelection}`);
     }
     else if(playerWins) {
-        decision = `Player won, ${playerSelection} beats ${computerSelection}`
+        decision = 1;
+        console.log(`Player won, ${playerSelection} beats ${computerSelection}`);
     }
     else {
-        decision = `Computer won, ${computerSelection} beats ${playerSelection}`
+        decision = 2;
+        console.log(`Computer won, ${computerSelection} beats ${playerSelection}`);
     }
 
-    console.log(`Player chose: ${playerSelection}, Computer chose: ${computerSelection}`)
-    return decision
+    console.log(`Player chose: ${playerSelection}, Computer chose: ${computerSelection}`);
+    return decision;
 }
 
 /**
  * Main Function to run the game
  */
 function main() {
-    const playerSelection = "scissors"
-    const computerSelection = "scissors"
-    console.log(playerRound(playerSelection, computerSelection))
+    const rounds = 5;
+    let winCount = 0; // number of wins
+    let loseCount = 0; //number of losses
+    let drawCount = 0; // number of draws
+
+    // Play the game for number of rounds, logging the outcome
+    for (let i = 0; i < rounds; i++) {
+        let playerSelection;
+        let computerSelection = getComputerChoice();
+        let invalid = true;
+        let decision;
+
+        while (invalid){
+            playerSelection = prompt("Enter 'rock', 'paper', or 'scissors'");
+
+            if (playerSelection){
+                playerSelection = playerSelection.toLowerCase();
+            }
+
+            if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors'){
+                invalid = false;
+            }
+            else {
+                alert('Entered wrong value. Try again');
+            }
+        }
+        
+        // Play round and update wins, losses, and draws
+        decision = playerRound(playerSelection, computerSelection);
+        if (decision === 1){
+            winCount++;
+        }
+        else if (decision === 2){
+            loseCount++;
+        }
+        else {
+            drawCount++;
+        }
+    }
+
+    // End game results
+    console.log(`Results: Wins: ${winCount}, Losses: ${loseCount}, Draws: ${drawCount}`)
+
+    if (winCount > 2){
+        console.log("You Win!!!!")
+    }
+    else if (loseCount > 2){
+        console.log("You Lost!!!!")
+    }
+    else {
+        console.log("It's a draw!!!!")
+    }
 }
 
 /**
